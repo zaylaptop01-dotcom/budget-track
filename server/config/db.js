1c60e6623a08e74ae9e8507f16e3abee030a02b0
Zay <zay.laptop01@gmail.com>
2026-03-28 22:24:59 +0900

    Fix database SSL config for production

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,11 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    rejectUnauthorized: false,
+    require: true
+  },
+  connectionTimeoutMillis: 5000,
 })
 
 pool.connect((err, client, release) => {
